90de14c08046cb66cede64ad5e72cd0ef8be0238
Iaroslav Volokh <vol@crtweb.ru>
2024-12-04 23:01:37 +0500

    market 195

--- market/index.js
+++ market/index.js
@@ -1,21 +1,14 @@
 e = (str) => new Date(str);
 
-p = (s, cnt = 2) => ('' + s).padStart(cnt, '0');
-
-process.argv
-  .slice(4)
-  .join(' ')
-  .split(' ')
+('' + process.argv.slice(4))
+  .split(/ |,/)
   .map((d) => d.split('.').reverse().join('-'))
   .sort()
   .map(
-    (a, i, s, date = e(e(a) - 259200000)) =>
-      i < 3 &&
-      console.log(
-        [
-          p(date.getDate()),
-          p(date.getMonth() + 1),
-          p(date.getFullYear(), 4),
-        ].join('.'),
-      ),
+    (a, i) => (
+      ([y, m, d] = e(e(a) - 259200000)
+        .toJSON()
+        .split(/-|T/)),
+      i < 3 && console.log(d + '.' + m + '.' + y)
+    ),
   );
