abdaca38c71a7e0426480d6c4a844b6d70db45fa
Iaroslav Volokh <vol@crtweb.ru>
2024-12-13 00:03:46 +0500

    concert 298

--- concert/index.js
+++ concert/index.js
@@ -6,19 +6,18 @@ T = c.slice(3);
 d = Array(M + 1).fill(0);
 l = [];
 
-r = 0;
-
 T.map((b) => {
   [o, y, v] = b.split(/ |:/);
   k = y * s + +v;
   for (let j = M; j >= k; j--) {
     if (d[j - k] + k > d[j]) {
       d[j] = d[j - k] + k;
-      r = d[j] > r ? d[j] : r;
       l[j] = [...(l[j - k] ?? []), o];
     }
   }
 });
+
+r = Math.max(...d);
 
 p = (s) => ('' + s).padStart(2, '0');
 console.log([...l[d.indexOf(r)], p((r / s) | 0) + ':' + p(r % s)].join`\n`);
