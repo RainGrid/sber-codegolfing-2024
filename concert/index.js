3183b50b2d204a815beebd7247284c494b25da12
Iaroslav Volokh <vol@crtweb.ru>
2024-12-14 18:52:37 +0500

    concert 212

--- concert/index.js
+++ concert/index.js
@@ -1,21 +1,20 @@
 s = 60;
 c = process.argv;
 M = c.at(-1) * s;
-d = {};
-l = {};
+d = console.log;
+
+p = (x) => (x > 9 ? x : '0' + x);
 
 c.map((b) => {
   [o, y, v] = b.split(/ |:/);
-  k = y * s + +v;
+  k = +v + y * s;
   for (j = M; j >= k; j--) {
     f = +[d[j - k]] + k;
     if (f > [d[j]]) {
       d[j] = f;
-      l[j] = [l[j - k]] + o + '\n';
+      p[j] = [p[j - k]] + o + '\n';
     }
   }
 });
 
-p = (x) => (x > 9 ? x : '0' + x);
-
-console.log(l[M] + p((d[M] / s) | 0) + ':' + p(d[M] % s));
+d(p[M] + p((d[M] / s) | 0) + ':' + p(d[M] % s));
