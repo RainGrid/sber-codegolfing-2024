7f42c5d80462e40a25b7bb0382bb2293f9ab5768
Iaroslav Volokh <vol@crtweb.ru>
2024-12-14 14:48:03 +0500

    concert 222

--- concert/index.js
+++ concert/index.js
@@ -1,23 +1,21 @@
 s = 60;
 c = process.argv;
+M = c.at(-1) * s;
+d = {};
+l = {};
 
-d = [];
-l = [];
-
-c.map((b) => {
+for (b of c) {
   [o, y, v] = b.split(/ |:/);
   k = y * s + +v;
-  for (j = c.at(-1) * s; j >= k; ) {
-    f = (d[--j - k] ?? 0) + k;
-    if (f > (d[j] ?? 0)) {
+  for (j = M; j >= k; j--) {
+    f = d[j - k] + k || k;
+    if (f > (d[j] || 0)) {
       d[j] = f;
-      l[j] = (l[j - k] ?? '') + o + '\n';
+      l[j] = [l[j - k]] + o + '\n';
     }
   }
-});
+}
 
-r = d.pop();
+p = (x) => (x > 9 ? x : '0' + x);
 
-p = (x) => (x < 10 ? '0' + x : x);
-
-console.log(l.pop() + p((r / s) | 0) + ':' + p(r % s));
+console.log(l[M] + p((d[M] / s) | 0) + ':' + p(d[M] % s));
